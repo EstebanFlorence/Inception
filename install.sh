faa4f740c2404b2369a50bbba185ffb757da9806
esteban florence <esteban@debian.homenet.telecomitalia.it>
2024-06-22 21:42:44 +0200

    Host Configuration Innit

---
+++ install.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+openssl version > /dev/null 2>&1
+
+if [ $? -ne 0 ]; then
+    echo "OpenSSL is not installed. Installing OpenSSL..."
+
+    if [ -x "$(command -v apk)" ]; then
+        sudo apk update && sudo apk install -y openssl
+        echo "OpenSSL has been installed successfully."
+    else
+        echo "Unsupported package manager. Please install OpenSSL manually."
+        # exit 1
+    fi
+fi
+
+
+if ! grep -q "127.0.0.1 $USER.42.fr" /etc/hosts; then
+    echo "Setting domain name as $USER.42.fr..."
+    echo "127.0.0.1 $USER.42.fr" | sudo tee -a /etc/hosts > /dev/null
+fi
+
+
+VOLUMES="/home/$USER/data"
+CERTS="/home/$USER/certs"
+
+mkdir -p $VOLUMES/db $VOLUMES/wp $CERTS
+
+openssl req -x509 -newkey sha:4096 -days 365 -nodes \
+        -out $CERTS/$USER.crt \
+        -keyout $CERTS/$USER.key \
+        -subj "/CO=IT/L=FI/O=42/OU=student/CN=$USER.42.fr"
+
+if [ $? -eq 0 ]; then
+    echo "OpenSSL certificates generated in $CERTS"
+    echo "Volumes available in $VOLUMES folder."
+else
+    echo "OpenSSL command failed."
+fi

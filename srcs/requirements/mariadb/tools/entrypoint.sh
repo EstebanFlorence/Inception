1eae68149a82efc5be9dc011bac5a27ca8984630
EstebanFlorence <asap.bacco@protonmail.com>
2024-07-02 14:22:25 +0200

    MariaDB

--- srcs/requirements/mariadb/tools/entrypoint.sh
+++ srcs/requirements/mariadb/tools/entrypoint.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-if [ ! -d /var/lib/mysql/wp ]; then
-    mariadb-install-db --default-file=/my.cnf.d/inception.cnf
+if [ ! -d /var/lib/mysql/mysql ]; then
+    mariadb-install-db --defaults-file=/etc/my.cnf.d/inception.cnf
 fi
